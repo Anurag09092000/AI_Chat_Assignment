c8fc565ed9fcbee0c01d6d9ef6d199b1a0063364
Anurag Sharma <145134717+Anurag09092000@users.noreply.github.com>
2024-03-21 08:24:24 +0000

    may i help you

--- src/MessageDisplay.jsx
+++ src/MessageDisplay.jsx
@@ -2,37 +2,58 @@ import React, { useEffect, useState } from "react";
 
 function MessageDisplay() {
   const [messages, setMessages] = useState([]);
-  const [fetchData,setFetchData] = useState(false);
-  
+  const [fetchData, setFetchData] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+
   const handleApi = async () => {
     const response = await fetch(
-      "https://www.dev.readychatai.com/messages_json?format=json"
+      "https://www.dev.readychatai.com/messages_json?format=json",
     );
     const data = await response.json();
     setMessages(data);
+    setShowButton(false);
   };
 
   useEffect(() => {
-    if(fetchData == true){
-        handleApi();
+    if (fetchData == true) {
+      handleApi();
     }
   }, [fetchData]);
 
   return (
     <div className="chat-container">
-        <div className = "heading">
-        <h1 >AI Assistant </h1>
-        <hr/>
-        </div>
-        <span> <button className = "btn" onClick = {()=> setFetchData(true)}> Show The Data</button>
-        <button className = "btn" onClick = {()=> setFetchData(false)}> Hide The Data</button>
-         </span>
-      {fetchData && messages.map((message) => (
-        <div key={message.id} className={message.sender_name === "bot" ? "message bot-message" : "message user-message"}>
-          <div className="emoji">{message.sender_name === "bot" ? "🤖" : "👦"}</div>
-          <p>{message.message_text}</p>
-        </div>
-      ))}
+      <div className="heading">
+        <h1>AI Assistant </h1>
+        <hr />
+      </div>
+      <span>
+        {showButton && (
+          <button className="btn" onClick={() => setFetchData(true)}>
+            May I Help You?
+          </button>
+        )}
+        {/* <button className="btn" onClick={() => setFetchData(false)}>
+          Hide The Data
+        </button> */}
+      </span>
+      {fetchData &&
+        messages.map((message) => (
+          <div
+            key={message.id}
+            className={
+              message.sender_name === "bot"
+                ? "message bot-message"
+                : "message user-message"
+            }
+          >
+            <div className="message-emoji">
+              <p className="emoji">
+                {message.sender_name === "bot" ? "🤖" : "👦"}
+              </p>
+              <p className="message-text">{message.message_text}</p>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
